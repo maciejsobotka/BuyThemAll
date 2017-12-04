cda8ee6aff04fb4c178bffaccdad09a117edd25b
maciejsobotka <maciejsobot@gmail.com>
2017-12-05 00:11:27 +0100

    Cart component.

--- buy-them-all/src/app/components/cart/cart.component.ts
+++ buy-them-all/src/app/components/cart/cart.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../../shared/services/cart.service';
+import { IProduct } from '../../shared/models/product'
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
+  Products: IProduct[];
 
-  constructor() { }
-
-  ngOnInit() {
+  get IsScreenXs(): boolean {
+    return window.innerWidth <= 600;
   }
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    this.Products = this.cartService.retriveCart();
+  }
+
+  orderPrice(): number {
+    return this.Products.reduce((a, b) => a + this.productPrice(b), 0);
+  }
+
+  productPrice(product: IProduct): number {
+    if (product.IsDiscounted) {
+      return product.Price - product.Price * product.DiscountPercent / 100;
+    }
+    return product.Price;
+  }
+
+  removeItem(index: number) {
+    this.Products.splice(index, 1);
+    this.cartService.removeFromCart(index);
+  }
 }
--- buy-them-all/src/app/shared/utils/data-helper.ts
+++ buy-them-all/src/app/shared/utils/data-helper.ts
@@ -2,4 +2,8 @@
     static hasValue(value: any) {
         return value !== undefined && value !== null && value !== NaN;
     }
+
+    static toLocaleString(value: number): string {
+        return value.toLocaleString('pl-PL', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    }
 }
